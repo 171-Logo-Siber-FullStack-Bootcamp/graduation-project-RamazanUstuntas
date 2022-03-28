b0bd51facd4e1282cf4884c130cfd4d1543226d4
RamazanUstuntas <ustuntasramazan@outlook.com>
2022-03-28 20:35:10 +0300

    create logger with winston

---
+++ backend-api/app/utils/logger.js
@@ -0,0 +1,40 @@
+const { createLogger, format, transports } = require('winston');
+
+serverLogger = createLogger({
+  transports:
+      new transports.File({
+      filename: 'app/logs/server.log',
+      format:format.combine(
+          format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+          format.align(),
+          format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+      )}),
+});
+
+usersLogger = createLogger({
+  transports:
+      new transports.File({
+      filename: 'app/logs/users.log',
+      format:format.combine(
+          format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+          format.align(),
+          format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+      )}),
+});
+
+transactionLogger = createLogger({
+  transports:
+      new transports.File({
+      filename: 'app/logs/transactions.log',
+      format:format.combine(
+          format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+          format.align(),
+          format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+      )}),
+});
+
+module.exports = {
+  serverLogger,
+  usersLogger,
+  transactionLogger,
+}
